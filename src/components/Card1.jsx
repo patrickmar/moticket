db339c0ea1bdb61d785c95852103914a8c30bfd4
sundayuchegbu <sundayuchegbu10@gmail.com>
2023-12-18 11:41:42 +0100

    brush up

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
 import { p1, p2, p3, p4, p5, p6, p7 } from '../assets';
 import { FaLocationDot, FaTicket } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 import { FaMoneyBill } from 'react-icons/fa';
+import { BsBuildings } from 'react-icons/bs';
 
 const data = [
   {
     title: 'The Idea event',
     location: 'Lagos',
     img: p1,
+    price: 5,
     icon: FaTicket,
+    date: new Date(2023, 11, 16), // December is represented by 11 (0-indexed)
   },
   {
     title: 'The Idea event1',
     location: 'Lagos',
     price: 5,
-
+    date: new Date(2023, 11, 16), // December is represented by 11 (0-indexed)
     img: p2,
     icon: FaTicket,
   },
@@ -27,7 +29,7 @@ const data = [
     title: 'The Idea event2',
     location: 'Lagos',
     price: 5,
-
+    date: new Date(2023, 11, 16), // December is represented by 11 (0-indexed)
     img: p3,
     icon: FaTicket,
   },
@@ -35,7 +37,7 @@ const data = [
     title: 'Event 4',
     location: 'Lagos',
     price: 5,
-
+    date: new Date(2023, 11, 16), // December is represented by 11 (0-indexed)
     img: p4,
     icon: FaTicket,
   },
@@ -43,7 +45,7 @@ const data = [
     title: 'event 5',
     location: 'Lagos',
     price: 5,
-
+    date: new Date(2023, 11, 16), // December is represented by 11 (0-indexed)
     img: p5,
     icon: FaTicket,
   },
@@ -51,14 +53,14 @@ const data = [
     title: 'event6',
     location: 'Lagos',
     price: 5,
-
+    date: new Date(2023, 11, 16), // December is represented by 11 (0-indexed)
     img: p6,
     icon: FaTicket,
   },
   {
     title: 'The Idea event7',
     location: 'Lagos',
-
+    date: new Date(2023, 11, 16), // December is represented by 11 (0-indexed)
     img: p7,
     icon: FaTicket,
     price: 5,
@@ -66,44 +68,80 @@ const data = [
 ];
 
 const Card1 = () => {
+  const CustomNextArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <button
+        className="slick-next"
+        style={{
+          background: 'black', // Set the background color to black or any other desired color
+          borderRadius: '50%', // Optional: To make it circular
+          height: '40px',
+          width: '40px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          border: 'none',
+          outline: 'none',
+          cursor: 'pointer',
+        }}
+        onClick={onClick}
+      ></button>
+    );
+  };
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
+    nextArrow: <CustomNextArrow />,
+  };
+
+  const formatDate = (date) => {
+    const options = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    };
+    return date.toLocaleDateString('en-GB', options);
   };
 
   return (
-    <section className="bg-gray-200">
+    <section className="">
       <h1 className="absolute mt-[-150px] font-semibold text-[24px]">
         Events This Week
       </h1>
 
-      <div className="w-3/4 m-auto mt-56 bg-gray-100 rounded-xl shadow-xl border-gray-500">
+      <div className="w-3/4 m-auto mt-56 rounded-xl  border">
         <div className="mt-20 ">
           <Slider {...settings}>
             {data.map((d, index) => (
               <div
                 key={index}
-                className="bg-gray-200 h-[450px] text-black rounded-xl"
+                className="bg-gray-200 h-[450px] text-black rounded-xl shadow-md relative border-r-4 border-gray-400 mb-8"
               >
-                <div className="h-56  rounded-xl flex justify-center mb-4 items-center">
+                <div className="h-56  rounded-t-xl flex justify-center mb-4 items-center">
                   <img src={d.img} alt="" className="h-60 w-full" />
                 </div>
+                <p>{formatDate(d.date)}</p>
+
                 <div className="flex  items-start gap-4 p-2 ">
-                  <FaTicket /> <p>{d.title}</p>
+                  <BsBuildings className="text-[#90cdf4]" /> <p>{d.title}</p>
                   {/* Assuming d.icon is a React component */}
                 </div>
                 <div className="flex  items-start gap-4 p-2 ">
-                  <FaLocationDot /> <p>{d.location}</p>
+                  <FaLocationDot className="text-[#90cdf4] w-5 h-5" />{' '}
+                  <p>{d.location}</p>
                   {/* Assuming d.icon is a React component */}
                 </div>
                 <div className="flex  items-start gap-4 p-2 ">
-                  <FaMoneyBill /> <p>£{d.price}</p>
+                  <p>£{d.price}</p>
                   {/* Assuming d.icon is a React component */}
                 </div>
-                <div lassName="flex  items-start ">
+                <div className="relative top-2 -left-20">
                   {' '}
                   <Link to="/cart">
                     <button className="bg-secondary p-1 text-white rounded-xl">
